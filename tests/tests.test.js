0134dc3a1965706a849e2fab1d5a36c736360960
dChancellor <me@chancellor.tech>
2021-07-25 01:50:26 -0500

    Trying this way

--- tests/tests.test.js
+++ tests/tests.test.js
@@ -4,6 +4,6 @@ const secretNumber = require("../config");
 describe('it tests github actions', () => {
     it('will multiply a given number by 2', () => {
         console.log(secretNumber);
-        expect(multiplyNumberByTwo(secretNumber)).toEqual(secretNumber*2)
+        expect(multiplyNumberByTwo(secretNumber)).toEqual(20)
     })
 })
